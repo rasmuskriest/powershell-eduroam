3078565a137b1d84729a785739bfcd90f7b35f07
Rasmus Kriest <rasmusk@outlook.com>
2017-12-21 14:29:17 +0100

    Add comments

--- Install-Eduroam.ps1
+++ Install-Eduroam.ps1
@@ -8,8 +8,10 @@
 #>
 #Requires -Version 3.0
 
+# Warning: All files with the ending .crt are installed automatically in the Root-Certificate Store. Please check whether only trusted certificates are in the folder.
 Get-ChildItem "$PSScriptRoot\certs" -Filter *.crt | Foreach-Object {
     Import-Certificate -FilePath $_.FullName -CertStoreLocation 'Cert:\LocalMachine\Root\' -Verbose
 }
 
+# Change the path to the *.xml you want to use as a profile. The included wlan_prof-0-comments.xml could work as a template.
 netsh wlan add profile filename="$PSScriptRoot\wlan_prof-0.xml" user=all
